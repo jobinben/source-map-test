ea51db988d1bf7d43818393dfbbc06cdec0cbcb1
jobinben <1021137079@qq.com>
2023-03-13 23:55:57 +0800

    修改sourcemap源代码输出

--- test/testMap.js
+++ test/testMap.js
@@ -6,14 +6,12 @@ const data = fs.readFileSync("../dist/bundle.js.map").toString();
 const consumer = new sourceMap.SourceMapConsumer(data);
 
 consumer.then(c => {
-    const line = 8;
-    const column = 100;
-    const s = c.originalPositionFor({line, column});
-    console.log('origin code for line: ', line, ', column: ', column);
+    const s = c.originalPositionFor({line: 1, column: 0});
+    // console.log('origin code for line: ', line, ', column: ', column);
     console.log('----------------------');
 
     // 输出原代码的8行到14行
-    console.log(c.sourceContentFor(s.source).split("\n").slice(line, line + 6).join("\n"));
+    console.log(c.sourceContentFor(s.source).split("\n").slice(8, 14).join("\n"));
 
     console.log('----------------------');
 })
