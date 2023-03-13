7a43bfea24afe0db2cae7b4ba159f060f20636a3
jobinben <1021137079@qq.com>
2023-03-13 23:40:20 +0800

    使用rollup打包出source-map

---
+++ src/main.js
@@ -0,0 +1,17 @@
+const fn = () => {
+    console.log('this is a fn');
+}
+
+
+const str = "this is a string variable.";
+
+
+const runFn = (str) => {
+    setTimeout(() => {
+        console.log('str: ' +　str);
+        throw new Error('this is async error');
+    }, 2e3);
+}
+
+
+runFn(str);
