eacb1e8acd21441dffa0bbc0a5b4c74c573fecf5
jobinben <1021137079@qq.com>
2023-03-14 22:57:09 +0800

    增加onerror获取error的line和column

--- test/testMap.js
+++ test/testMap.js
@@ -1,17 +1,23 @@
 // 用cjs
-const sourceMap = require("source-map");
-const fs = require("fs");
+// const sourceMap = require("source-map");
+// const fs = require("fs");
+import sourceMap from "source-map";
+import fs from "fs";
 
 const data = fs.readFileSync("../dist/bundle.js.map").toString();
 const consumer = new sourceMap.SourceMapConsumer(data);
 
-consumer.then(c => {
-    const s = c.originalPositionFor({line: 1, column: 0});
-    // console.log('origin code for line: ', line, ', column: ', column);
-    console.log('----------------------');
+const GetLocation = (line, column) => {
+    consumer.then(c => {
+        const s = c.originalPositionFor({line, column});
+        console.log('origin code for line: ', line, ', column: ', column);
+        console.log('----------------------');
+    
+        // 输出原代码的第line行到line + 5行
+        console.log(c.sourceContentFor(s.source).split("\n").slice(line, line + 5).join("\n"));
+    
+        console.log('----------------------');
+    })
+}
 
-    // 输出原代码的8行到14行
-    console.log(c.sourceContentFor(s.source).split("\n").slice(8, 14).join("\n"));
-
-    console.log('----------------------');
-})
+GetLocation(7, 15);
